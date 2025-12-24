61957f8321d84cdaf0ee991ccf01fbfe20878afe
Jeremiah Mateo <jeremiah.mateo4321@gmail.com>
2025-12-24 23:33:45 +0400

    Added function handleMove(event). Event listener now executes this funciont instead of an anonymous one. Also added removeEvenListener when win condition

--- Box Pusher/scripts/game.js
+++ Box Pusher/scripts/game.js
@@ -1,6 +1,6 @@
-const rand = 3 + Math.ceil(Math.random() * 3);
+const arenaSize = 2 + Math.ceil(Math.random() * 3);
 const floor = document.getElementById('floor');
-const move = document.querySelector('.text');
+const retryBtn = document.querySelector('.retry-btn');
 let tile;
 let box;
 let target;
@@ -8,11 +8,11 @@ let keyPress;
 let row;
 let col;
 
-for (let i = 0; i < rand; i++) {
+for (let i = 0; i < arenaSize; i++) {
   const rowDiv = document.createElement('div');
   rowDiv.className = `${i} row`;
 
-  for (let j = 0; j < rand; j++) {
+  for (let j = 0; j < arenaSize; j++) {
     tile = document.createElement('div');
     tile.className = `tile`;
     tile.dataset.row = i;
@@ -23,7 +23,7 @@ for (let i = 0; i < rand; i++) {
   floor.appendChild(rowDiv);
 }
 
-target = document.querySelector(`[data-row="${rand-1}"][data-col="${rand-1}"]`);
+target = document.querySelector(`[data-row="${arenaSize-1}"][data-col="${arenaSize-1}"]`);
 target.classList.add('target');
 
 box = document.querySelector('[data-row="0"][data-col="0"]');
@@ -32,46 +32,46 @@ box.classList.add('box');
 row = Number(box.dataset.row);
 col = Number(box.dataset.col);
 
-document.addEventListener('keydown', (event) => {
+document.addEventListener('keydown', handleMove);
+
+retryBtn.addEventListener('click', () => {
+  location.reload();
+});
+
+function handleMove(event) {
   keyPress = event.key;
   if (keyPress.includes('Arrow')) event.preventDefault();
 
   if (keyPress.includes('Right')) {
-    col === rand - 1 ? col = 0 : col++;
+    col === arenaSize - 1 ? col = 0 : col++;
     box.classList.remove('box');
     box = document.querySelector(`[data-row="${row}"][data-col="${col}"]`)
     box.classList.add('box');
   }
 
   if (keyPress.includes('Left')) {
-    col === 0 ? col = rand - 1 : col--;
+    col === 0 ? col = arenaSize - 1 : col--;
     box.classList.remove('box');
     box = document.querySelector(`[data-row="${row}"][data-col="${col}"]`)
     box.classList.add('box');
   }
 
   if (keyPress.includes('Up')) {
-    row === 0 ? row = rand - 1 : row--;
+    row === 0 ? row = arenaSize - 1 : row--;
     box.classList.remove('box');
     box = document.querySelector(`[data-row="${row}"][data-col="${col}"]`)
     box.classList.add('box');
   }
 
   if (keyPress.includes('Down')) {
-    row === rand - 1 ? row = 0 : row++;
+    row === arenaSize - 1 ? row = 0 : row++;
     box.classList.remove('box');
     box = document.querySelector(`[data-row="${row}"][data-col="${col}"]`)
     box.classList.add('box');
   }
 
   if (box.dataset.row === target.dataset.row && box.dataset.col === target.dataset.col) {
-    move.innerHTML = `You win!`;
-    location.reload();
+    document.removeEventListener('keydown', handleMove);
+    console.log('You win!');
   }
-
-  move.innerHTML = keyPress;
-});
-
-
-
-
+}
