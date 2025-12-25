55a765d321d9b1607eab8f879e52431f6c14133d
Jeremiah Mateo <jeremiah.mateo4321@gmail.com>
2025-12-25 16:16:28 +0400

    Ok. Separate it into game and arena

--- Box Pusher/scripts/game.js
+++ Box Pusher/scripts/game.js
@@ -1,57 +1,20 @@
+import { buildArena } from "./arena.js";
+// import { handleMove } from "./input.js";
+
 const arenaSize = Math.floor(Math.random() * 10) + 20;
-const floor = document.getElementById('floor');
-const floorHeight = floor.offsetHeight;
-const floorWidth = floor.offsetWidth;
-
-let tile;
 let box;
-// let obstacle;
-let obstacleRandRow;
-let obstacleRandCol;
-let target;
-let keyPress;
-let row;
-let col;
 
-for (let i = 0; i < arenaSize; i++) {
-  const rowDiv = document.createElement('div');
-  rowDiv.className = `${i} row`;
+buildArena(arenaSize);
 
-  for (let j = 0; j < arenaSize; j++) {
-    tile = document.createElement('div');
-    tile.style.height = floorHeight / arenaSize * 0.9 + 'px';
-    tile.style.width = floorWidth / arenaSize * 0.9 + 'px';
-    tile.className = `tile`;
-    tile.dataset.row = i;
-    tile.dataset.col = j;
-
-    rowDiv.appendChild(tile);
-  }
-  floor.appendChild(rowDiv);
-}
-
-for (let i = 0; i < arenaSize * 2; i++) {
-  obstacleRandRow = Math.floor(Math.random() * arenaSize);
-  obstacleRandCol = Math.floor(Math.random() * arenaSize);
-
-  if ((obstacleRandRow === 0 && obstacleRandCol === 0) ||
-      (obstacleRandRow === arenaSize - 1 && obstacleRandCol === arenaSize - 1)
-  ) {
-    continue;
-  }
-
-  obstacle = document.querySelector(`[data-row="${obstacleRandRow}"][data-col="${obstacleRandCol}"]`);
-
-  if (!obstacle) continue;
-
-  obstacle.classList.add('obstacle');
-}
-
-target = document.querySelector(`[data-row="${arenaSize-1}"][data-col="${arenaSize-1}"]`);
+const target = document.querySelector(`[data-row="${arenaSize-1}"][data-col="${arenaSize-1}"]`);
 target.classList.add('target');
 
 box = document.querySelector('[data-row="0"][data-col="0"]');
 box.classList.add('box');
+
+let keyPress;
+let row;
+let col;
 
 row = Number(box.dataset.row);
 col = Number(box.dataset.col);
@@ -141,3 +104,5 @@ function handleMove(event) {
     console.log('You win!');
   }
 }
+
+
---
+++ Box Pusher/scripts/input.js
@@ -0,0 +1,2 @@
+import { box } from './game.js';
+
