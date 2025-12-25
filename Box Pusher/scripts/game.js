1c8e26bf95149ae40976846e7738d2f48fab4fc1
Jeremiah Mateo <jeremiah.mateo4321@gmail.com>
2025-12-25 04:28:43 +0400

    Made arena bigger. Tile dimensions now adjust to arena size

--- Box Pusher/scripts/game.js
+++ Box Pusher/scripts/game.js
@@ -1,6 +1,9 @@
-const arenaSize = 2 + Math.ceil(Math.random() * 3);
+const arenaSize = 5 + Math.ceil(Math.random() * 10);
 const floor = document.getElementById('floor');
-const retryBtn = document.querySelector('.retry-btn');
+const floorHeight = floor.offsetHeight;
+const floorWidth = floor.offsetWidth;
+
+// const retryBtn = document.querySelector('.retry-btn');
 let tile;
 let box;
 let target;
@@ -14,6 +17,8 @@ for (let i = 0; i < arenaSize; i++) {
 
   for (let j = 0; j < arenaSize; j++) {
     tile = document.createElement('div');
+    tile.style.height = floorHeight / arenaSize / 1.1+ 'px';
+    tile.style.width = floorWidth / arenaSize + 'px';
     tile.className = `tile`;
     tile.dataset.row = i;
     tile.dataset.col = j;
@@ -22,6 +27,8 @@ for (let i = 0; i < arenaSize; i++) {
   }
   floor.appendChild(rowDiv);
 }
+
+
 
 target = document.querySelector(`[data-row="${arenaSize-1}"][data-col="${arenaSize-1}"]`);
 target.classList.add('target');
@@ -34,37 +41,37 @@ col = Number(box.dataset.col);
 
 document.addEventListener('keydown', handleMove);
 
-retryBtn.addEventListener('click', () => {
-  location.reload();
-});
+// retryBtn.addEventListener('click', () => {
+//   location.reload();
+// });
 
 function handleMove(event) {
   keyPress = event.key;
   if (keyPress.includes('Arrow')) event.preventDefault();
 
   if (keyPress.includes('Right')) {
-    col === arenaSize - 1 ? col = 0 : col++;
+    col === arenaSize - 1 ? col = col : col++;
     box.classList.remove('box');
     box = document.querySelector(`[data-row="${row}"][data-col="${col}"]`)
     box.classList.add('box');
   }
 
   if (keyPress.includes('Left')) {
-    col === 0 ? col = arenaSize - 1 : col--;
+    col === 0 ? col = col : col--;
     box.classList.remove('box');
     box = document.querySelector(`[data-row="${row}"][data-col="${col}"]`)
     box.classList.add('box');
   }
 
   if (keyPress.includes('Up')) {
-    row === 0 ? row = arenaSize - 1 : row--;
+    row === 0 ? row = row : row--;
     box.classList.remove('box');
     box = document.querySelector(`[data-row="${row}"][data-col="${col}"]`)
     box.classList.add('box');
   }
 
   if (keyPress.includes('Down')) {
-    row === arenaSize - 1 ? row = 0 : row++;
+    row === arenaSize - 1 ? row = row : row++;
     box.classList.remove('box');
     box = document.querySelector(`[data-row="${row}"][data-col="${col}"]`)
     box.classList.add('box');
@@ -72,6 +79,12 @@ function handleMove(event) {
 
   if (box.dataset.row === target.dataset.row && box.dataset.col === target.dataset.col) {
     document.removeEventListener('keydown', handleMove);
+    document.addEventListener('keydown', (event) => {
+      if (event.key === ('Enter')) {
+        location.reload();
+      }
+    });
+    target.classList.add('win-tile');
     console.log('You win!');
   }
 }
