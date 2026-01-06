f5dc3539ccc8158e78e052d02c05e27085d15394
Jeremiah Mateo <jeremiah.mateo4321@gmail.com>
2026-01-06 17:36:07 +0400

    Changed dictionary api. This one now supports 'kites'. Hopefully more extensive

--- Wordle/scripts/validWordCheck.js
+++ Wordle/scripts/validWordCheck.js
@@ -1,11 +1,18 @@
 async function checkIfValidWord(word) {
-  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+  const url = `https://freedictionaryapi.com/api/v1/entries/en/${word.toLowerCase()}`;
+  // const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
   
   const response = await fetch(url);
   
-  if (!response.ok) {
-    return false;
-  } else {
-    return true;
-  }
+  // For https://freedictionaryapi.com/api/v1/entries/en/${word}
+  const result = await response.json();
+  const entries = result.entries;
+  console.log(result);
+  
+  if (!entries || entries.length === 0) return false;
+  
+  // For https://api.dictionaryapi.dev/api/v2/entries/en/${word}
+  // if (!response.ok) return false;
+  
+  return true;
 }
