d612f2aa4272f1b34131260b803e501a99407cdf
Jeremiah Mateo <jeremiah.mateo4321@gmail.com>
2026-01-06 17:38:54 +0400

    Simplified if statement in validWordCheck.js. Why am i even making a message for this. Just look at the commit

--- Wordle/scripts/validWordCheck.js
+++ Wordle/scripts/validWordCheck.js
@@ -7,9 +7,8 @@ async function checkIfValidWord(word) {
   // For https://freedictionaryapi.com/api/v1/entries/en/${word}
   const result = await response.json();
   const entries = result.entries;
-  console.log(result);
   
-  if (!entries || entries.length === 0) return false;
+  if (entries.length === 0) return false;
   
   // For https://api.dictionaryapi.dev/api/v2/entries/en/${word}
   // if (!response.ok) return false;
